10c3efd2183a3340fd5bb2edc75e74a3df84d1bd
Yuvaraj Sakthivel <32687822+uvcourses@users.noreply.github.com>
2020-11-14 18:40:22 -0600

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("Hello World");  
+		
+		for(int i=0;i<=10;i++)
 	System.out.println("Jenkin Build on commi.  t "); 	  
 		
 	}
